e8d2e367ee2049b578076486a857f6469fa56e04
Vic-kij <viictoria.bottaro@gmail.com>
2024-11-29 15:48:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aggiungi funzionalità come animazioni, validazioni, ecc.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Grazie per averci contattato!');
+});
